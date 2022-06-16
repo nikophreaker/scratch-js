673909b94a202e75775eab14f7dc555be092333b
NikxPhreaker <nikx449@gmail.com>
2022-06-16 14:55:37 +0700

    update api consume

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 const DIST_DIR = path.join(__dirname, "/");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
+const HTML_FILE = path.join(DIST_DIR, "index.ejs");
 
 app.use(express.static(DIST_DIR));
-app.get("*", (req, res) => {
-    res.sendFile(HTML_FILE);
+app.engine('html', require('ejs').renderFile);
+app.get('*', (req, res) => {
+    // res.sendFile(HTML_FILE);
+    res.render(HTML_FILE, {
+        token: req.query.token
+    })
 });
 
 app.listen(PORT, () => {
